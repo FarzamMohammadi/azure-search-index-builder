b04c7a787e84c4660ebda340e81e20b4f4ff051d
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-12-18 17:45:55 -0500

    Get books from JSON file & insert into Azure Storage Table

---
+++ AzureSearchIndexBuilder/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace AzureSearchIndexBuilder.Models;
+
+public class Book : ITableEntity
+{
+    public string PartitionKey { get; set; } = null!;
+    public string RowKey { get; set; } = null!;
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string Genre { get; set; } = null!;
+    public int PublishedYear { get; set; }
+    public double Price { get; set; }
+}
+
+// Sortable Fields: PublishedYear and Price. These fields are numerical and inherently sortable.
+// Filterable Field: Genre. It's a categorical field suitable for filtering.
+// Searchable Fields: Title and Author. These are textual fields, perfect for full-text search.
--- AzureSearchIndexBuilder/Program.cs
+++ AzureSearchIndexBuilder/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Azure Storage Table creation
+// Do this in portal
+
+// Table creation and data insertion
+
+using Azure.Data.Tables;
+using AzureSearchIndexBuilder.Models;
+using Newtonsoft.Json;
+
+var serviceClient = new TableServiceClient
+(
+    new Uri("TABLE_ENDPOINT - E.G: https://{STORAGE_ACCOUNT_NAME}.table.core.windows.net/"),
+    new TableSharedKeyCredential
+    (
+        "STORAGE_ACCOUNT_NAME - Displayed at the very top of the 'Storage Account' overview.",
+        "STORAGE_ACCOUNT_KEY - Available in the portal under 'Access Keys'"
+    )
+);
+
+const string tableName = "Books";
+serviceClient.CreateTableIfNotExists(tableName);
+
+var tableClient = serviceClient.GetTableClient(tableName);
+
+var jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "books.json");
+var jsonFileContent = File.ReadAllText(jsonFilePath);
+var books = JsonConvert.DeserializeObject<List<Book>>(jsonFileContent) ?? throw new Exception("Unable to deserialize books. Check JSON file and make sure it was included in the build.");
+
+foreach (var book in books)
+{
+    book.PartitionKey = book.Genre;
+    book.RowKey = book.Id.ToString();
+
+    await tableClient.AddEntityAsync(book);
+}
+
+// Create Entity (with filterable and sortable and whatever else fields)
+
+// Create Data Source Connection
+// Create Indexer
+// Create Index
+// Query data for the fields we setup in the index
+
+Console.WriteLine("sadf");
