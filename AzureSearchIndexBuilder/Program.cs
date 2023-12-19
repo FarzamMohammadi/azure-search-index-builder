3beb73f331c37fd1105676933549253c79c55bc3
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-12-18 20:02:32 -0500

    Create Data Source Connection & Indexer

--- AzureSearchIndexBuilder/Program.cs
+++ AzureSearchIndexBuilder/Program.cs
@@ -20,7 +20,7 @@ configuration.Bind(appSettings);
 var storageAccountName = appSettings.AzureStorageAccount.Name;
 var storageAccountKey = appSettings.AzureStorageAccount.Key;
 
-var serviceClient = new TableServiceClient
+var tableServiceClient = new TableServiceClient
 (
     new Uri($"https://{storageAccountName}.table.core.windows.net/"),
     new TableSharedKeyCredential
@@ -31,9 +31,9 @@ var serviceClient = new TableServiceClient
 );
 
 const string tableName = "Books";
-serviceClient.CreateTableIfNotExists(tableName);
+tableServiceClient.CreateTableIfNotExists(tableName);
 
-var tableClient = serviceClient.GetTableClient(tableName);
+var tableClient = tableServiceClient.GetTableClient(tableName);
 
 var jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "books.json");
 var jsonFileContent = File.ReadAllText(jsonFilePath);
@@ -44,7 +44,7 @@ foreach (var book in books)
     book.PartitionKey = book.Genre;
     book.RowKey = book.Id;
 
-    await tableClient.AddEntityAsync(book);
+    tableClient.AddEntity(book);
 }
 
 // Create Entity (with filterable and sortable and whatever else fields)
@@ -57,14 +57,34 @@ foreach (var book in books)
 var searchServiceEndpoint = appSettings.AzureSearchService.Url;
 var searchServiceAdminApiKey = appSettings.AzureSearchService.AdminApiKey;
 
-var searchIndexClient = new SearchIndexClient(new Uri(searchServiceEndpoint), new AzureKeyCredential(searchServiceAdminApiKey));
+var searchIndexClient =
+    new SearchIndexClient(new Uri(searchServiceEndpoint), new AzureKeyCredential(searchServiceAdminApiKey));
 
 var fieldBuilder = new FieldBuilder();
 var searchFields = fieldBuilder.Build(typeof(Book));
 
-var searchIndexDefinition = new SearchIndex("my-index", searchFields);
-searchIndexClient.CreateOrUpdateIndex(searchIndexDefinition);
+var searchIndex = new SearchIndex("my-index", searchFields);
+searchIndexClient.CreateOrUpdateIndex(searchIndex);
+
+// Create Data Source Connection
+var searchIndexerClient = new SearchIndexerClient(new Uri(searchServiceEndpoint), new AzureKeyCredential(searchServiceAdminApiKey));
+
+var dataSourceConnection = new SearchIndexerDataSourceConnection
+(
+    "my-index-data-source",
+    SearchIndexerDataSourceType.AzureTable,
+    $"DefaultEndpointsProtocol=https;AccountName={storageAccountName};AccountKey={storageAccountKey};EndpointSuffix=core.windows.net",
+    new SearchIndexerDataContainer(tableName)
+);
+
+searchIndexerClient.CreateOrUpdateDataSourceConnection(dataSourceConnection);
 
 // Create Indexer
-// Create Data Source Connection
+var searchIndexer = new SearchIndexer("my-index-indexer", dataSourceConnection.Name, searchIndex.Name);
+searchIndexerClient.CreateOrUpdateIndexer(searchIndexer);
+
+// Run Indexer
+searchIndexerClient.ResetIndexer(searchIndexer.Name);
+searchIndexerClient.RunIndexer(searchIndexer.Name);
+
 // Query data for the fields we setup in the index
